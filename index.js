01f28fc5e66e6e73d5d96e5993ae821076f7533c
Justin Escobar <crustinibustini2020@gmail.com>
2021-04-17 10:34:56 -0400

    commit tree 5e0e30d1c8e7c494a551070a3e84b716afee2c79

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+if (distance <= 400  ) {
+  return 'This one is on me!'
+} else if(distance >= 2000 && distance < 2500) {
+  return 'I will gladly take your thirty bucks.'
+} else { 
+return 'No can do.'
+}
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+
+  
 }
 
 function switchOnCharmFromTip(){
